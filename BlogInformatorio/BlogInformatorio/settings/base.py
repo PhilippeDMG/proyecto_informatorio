ced363510227a0173c146ecda55ad9f5954284b0
Philippe Maurel <92586430+PhilippeDMG@users.noreply.github.com>
2022-12-23 12:26:25 -0300

    Cambios para intento de deploy

--- BlogInformatorio/BlogInformatorio/settings/base.py
+++ BlogInformatorio/BlogInformatorio/settings/base.py
@@ -23,6 +23,28 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+SECRET_KEY = 'django-insecure-e6#zz@kcy7a+6_4@63@q(xvpkkkb1nq_39=6jv&1e@4o&c0w^v'
+
+DEBUG = True
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR),"static"),
+)
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mssql',
+        'NAME': 'informatorio',
+        'USER': 'sa',
+        'PASSWORD': 'sa',
+        'HOST': 'PHILIPPE\SQLEXPRESS',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        }
+    }
+}
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
